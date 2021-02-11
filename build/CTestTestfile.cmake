53b407ff048f8932885ac3d3fc934a3c62b3d1d9
Wahyu Mahardika <wmhrdk@gmail.com>
2021-02-11 21:33:45 +0800

    Reconstructing directories

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wmhrdk/Dev/blackturtle-robot/src
-# Build directory: /home/wmhrdk/Dev/blackturtle-robot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/wmhrdk/Dev/blackturtle-robot/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/wmhrdk/Dev/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/wmhrdk/Dev/catkin_ws/devel;/opt/ros/noetic' != '' else []
